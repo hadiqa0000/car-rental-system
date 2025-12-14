8efbd2eff6e5f8777ce442566f258b5f1f4c6ec7
Hadiqa Imran <hadiqaimrannn0@example.com>
2025-12-14 14:47:15 +0300

    getting ready the customer class to implement log in functionality

--- Customer.java
+++ Customer.java
@@ -2,18 +2,20 @@ public class Customer {
     private String id;
     private String name;
     private String email;
-    
-    public Customer(String id, String name, String email) {
+    private String password;
+
+    public Customer(String id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.password = password;
     }
-    
-    
+
     public String getId() { return id; }
     public String getName() { return name; }
     public String getEmail() { return email; }
-    
+    public boolean checkPassword(String pass) { return password.equals(pass); }
+
     @Override
     public String toString() {
         return name + " (" + id + ")";
